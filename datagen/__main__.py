4b63b85d74d4e5cb6abf3433f3e471a9be18a146
mertokten <mert.okten@icloud.com>
2025-04-15 21:33:00 -0400

    integrated extension class to main

--- datagen/__main__.py
+++ datagen/__main__.py
@@ -1,20 +1,27 @@
 import typer
 from typing import List, Optional
 from .reader import read_source_file
+from .utils import Extension
 
 app = typer.Typer()
 
 @app.command()
 def generate(
     input_file: str = typer.Option(..., help="Girdi dosyasının yolu"),
-    extension: str = typer.Option("csv", help="Dosya tipi: csv veya parquet"),
+    extension: Extension = typer.Option(
+        Extension.csv,
+        help='Girdi dosyasının uzantısı (csv)'
+    ),
     sep: str = typer.Option(",", help="CSV dosya ayıraç karakteri"),
-    excluded_cols: Optional[List[str]] = typer.Option(None, help="Hariç tutulacak kolonlar")
+    excluded_cols: Optional[List[str]] = typer.Option(
+        [],
+        help="Hariç tutulacak kolonlar"
+    ),
 ):
     excluded_cols = excluded_cols or []
     df = read_source_file(
         file_path=input_file,
-        extension=extension,
+        extension=extension.value,
         sep=sep,
         excluded_cols=excluded_cols
     )
