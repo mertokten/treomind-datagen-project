7de849b789be3088e7bdd5053acbd2b22986250b
mertokten <mert.okten@icloud.com>
2025-04-15 20:49:38 -0400

    added parquet support to read_source_file()

--- datagen/reader.py
+++ datagen/reader.py
@@ -1,4 +1,22 @@
+from typing import (
+    Literal, 
+    List
+)
+
 import pandas as pd
 
-def read_source_file(file_path: str, sep: str = ",") -> pd.DataFrame:
-    return pd.read_csv(file_path, sep=sep)
+
+def read_source_file(
+    file_path: str,
+    extension: Literal['csv', 'parquet'] = 'csv',
+    sep: str = ',',
+    excluded_cols: List[str] = []
+):
+    if extension == 'csv':
+        df = pd.read_csv(file_path, sep=sep)
+    elif extension == 'parquet':
+        df = pd.read_parquet(file_path, engine='auto')
+    else:
+        raise ValueError("Desteklenmeyen dosya türü: csv veya parquet olmalı")
+    
+    return df[[col for col in df.columns if col not in excluded_cols]]
